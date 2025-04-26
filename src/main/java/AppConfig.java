a71e32faae57f7f7423b11bac1f3ec1e537c034f
Sashakolenina <Sasha_kolenina@mail.ru>
2025-04-26 14:06:35 +0300

    first commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,11 +1,10 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
