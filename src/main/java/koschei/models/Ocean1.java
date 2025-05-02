575bed94a0ee68b9cff9a3bd7f9aaafaaaa0d773
Sashakolenina <Sasha_kolenina@mail.ru>
2025-05-02 18:37:55 +0300

    feature/bean_binding

---
+++ src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+    @Autowired
+    private Island2 island;
+
+    public Island2 getIsland() {
+        return island;
+    }
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island.toString();
+    }
+}
