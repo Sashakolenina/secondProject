c6615cc6e9e7c1207747c63b879f4dbf6fe807d0
Sashakolenina <Sasha_kolenina@mail.ru>
2025-05-17 20:13:44 +0300

    feature/bean_spring_mvc

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,11 @@
+package web.dao;
+import web.model.User;
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+    List<User> listUsers();
+    User getUserById(Long id);
+    void update(User user);
+    void delete(Long id);
+}
